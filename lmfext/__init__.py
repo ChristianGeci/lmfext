1cf49279e04d082f7806c635ec08cbc2a35060dd
Christian Geci <christian.geci@maine.edu>
2026-06-09 10:46:40 -0400

    initial commit

---
+++ lmfext/__init__.py
@@ -0,0 +1,6 @@
+from .core import *
+
+__version__ = "0.1.0"
+__author__ = 'Christian Geci'
+__credits__ = 'University of Maine'
+
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='lmfext',
+    version='0.1.0',    
+    description='extensions for the lmfit python package',
+    #url='https://github.com/ChristianGeci/fluorCalc',
+    author='Christian Geci',
+    author_email='christian.geci@maine.edu',
+    license='MIT license',
+    packages=['lmfext'],
+    install_requires=[
+                      'numpy',
+                      'lmfit',
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        #'License :: OSI Approved :: BSD License',  
+        #'Operating System :: POSIX :: Linux',        
+        #'Programming Language :: Python :: 2',
+        #'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        #'Programming Language :: Python :: 3.4',
+        #'Programming Language :: Python :: 3.5',
+    ],
+)
